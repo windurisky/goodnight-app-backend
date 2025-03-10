b4ec955a9deae361842e7cd40dd4e69606b9438e
Windu Risky Adiatma <windu.adiatma@fazzfinancial.com>
2025-03-10 02:46:17 +0000

    add relation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :following_relations, class_name: "Follow", foreign_key: "follower_id", inverse_of: :follower
+  has_many :follower_relations, class_name: "Follow", foreign_key: "followed_id", inverse_of: :followed
+
+  has_many :followings, -> { where(follows: { active: true }) }, through: :following_relations, source: :followed
+  has_many :followers, -> { where(follows: { active: true }) }, through: :follower_relations, source: :follower
+
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create
 
