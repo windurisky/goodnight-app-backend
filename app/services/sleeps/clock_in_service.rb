967d3a6133e4d464bd69ab4887b752cc25018e49
Windu Risky Adiatma <windu.adiatma@fazzfinancial.com>
2025-03-11 04:23:21 +0700

    simplify ClockInService validation

--- app/services/sleeps/clock_in_service.rb
+++ app/services/sleeps/clock_in_service.rb
@@ -12,12 +12,8 @@ module Sleeps
 
     private
 
-    def active_sleep_record
-      @active_sleep_record ||= @user.sleep_records.clocked_in.first
-    end
-
     def validate!
-      raise SleepError::AlreadyClockedIn if active_sleep_record.present?
+      raise SleepError::AlreadyClockedIn if @user.sleep_records.clocked_in.exists?
     end
   end
 end
