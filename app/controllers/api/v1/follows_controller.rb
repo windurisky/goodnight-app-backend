b7c9053ac257d3e608c59bd40dcd223cf5da25a2
Windu Risky Adiatma <windu.adiatma@fazzfinancial.com>
2025-03-10 09:30:05 +0000

    Update the endpoint structure and rename FollowsController into SocialsController

--- app/controllers/api/v1/follows_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  module V1
-    class FollowsController < Api::V1::BaseController
-      def create
-        # TODO: build the business logic
-      end
-    end
-  end
-end
---
+++ app/controllers/api/v1/socials_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class SocialsController < Api::V1::BaseController
+      def follow
+        # TODO: POST - follow logic
+      end
+
+      def unfollow
+        # TODO: POST - Unfollow logic
+      end
+
+      def followers
+        # TODO: GET - followers logic
+      end
+
+      def followings
+        # TODO: GET - followings logic
+      end
+    end
+  end
+end
