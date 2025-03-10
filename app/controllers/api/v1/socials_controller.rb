aa78437c55a02173d22636cc620f67bc3d1cdf45
Windu Risky Adiatma <windu.adiatma@fazzfinancial.com>
2025-03-10 17:34:05 +0000

    Fix the controller to match swagger API doc

--- app/controllers/api/v1/socials_controller.rb
+++ app/controllers/api/v1/socials_controller.rb
@@ -1,38 +1,30 @@
 module Api
   module V1
     class SocialsController < Api::V1::BaseController
+      before_action :authenticate_user!
+
       def follow
         ::Socials::FollowService.call(
           user: current_user,
-          target_user_id: follow_params[:user_id]
+          target_user_id: params[:user_id]
         )
 
-        render json: { message: "Successfully followed" }, status: :created
+        render json: { message: "Successfully followed user" }, status: :created
       end
 
       def unfollow
         ::Socials::UnfollowService.call(
           user: current_user,
-          target_user_id: unfollow_params[:user_id]
+          target_user_id: params[:user_id]
         )
 
-        render json: { message: "Successfully unfollowed" }, status: :ok
+        render json: { message: "Successfully unfollowed user" }, status: :ok
       end
 
       # TODO: GET - list of followers logic (low prio)
       def followers; end
       # TODO: GET - list of followings logic (low prio)
       def followings; end
-
-      private
-
-      def follow_params
-        params.require(:follow).permit(:user_id)
-      end
-
-      def unfollow_params
-        params.require(:unfollow).permit(:user_id)
-      end
     end
   end
 end
