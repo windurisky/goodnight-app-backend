43014da3c414ec8cfd438a2cfe46281a200568fe
Windu Risky Adiatma <windu.adiatma@fazzfinancial.com>
2025-03-10 11:02:13 +0000

    Complete the logic of SocialsController

--- app/controllers/api/v1/socials_controller.rb
+++ app/controllers/api/v1/socials_controller.rb
@@ -2,19 +2,36 @@ module Api
   module V1
     class SocialsController < Api::V1::BaseController
       def follow
-        # TODO: POST - follow logic
+        ::Socials::FollowService.call(
+          user: current_user,
+          target_user_id: follow_params[:user_id]
+        )
+
+        render json: { message: "Successfully followed" }, status: :created
       end
 
       def unfollow
-        # TODO: POST - Unfollow logic
+        ::Socials::UnfollowService.call(
+          user: current_user,
+          target_user_id: unfollow_params[:user_id]
+        )
+
+        render json: { message: "Successfully unfollowed" }, status: :ok
       end
 
-      def followers
-        # TODO: GET - followers logic
+      # TODO: GET - list of followers logic (low prio)
+      def followers; end
+      # TODO: GET - list of followings logic (low prio)
+      def followings; end
+
+      private
+
+      def follow_params
+        params.require(:follow).permit(:user_id)
       end
 
-      def followings
-        # TODO: GET - followings logic
+      def unfollow_params
+        params.require(:unfollow).permit(:user_id)
       end
     end
   end
