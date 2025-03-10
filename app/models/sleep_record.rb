fe090bb70bd2012e3abee2728a0ab151476e3e16
Windu Risky Adiatma <windu.adiatma@fazzfinancial.com>
2025-03-11 02:39:24 +0700

    add SleepRecord model

---
+++ app/models/sleep_record.rb
@@ -0,0 +1,32 @@
+class SleepRecord < ApplicationRecord
+  include AASM
+
+  before_create :generate_uuid_v7
+
+  belongs_to :user
+
+  validates :clocked_in_at, presence: true
+  validate :valid_clock_out_time, if: -> { clocked_out_at.present? }
+
+  scope :last_week, -> { where(clocked_in_at: 1.week.ago.beginning_of_day..) }
+
+  aasm column: :state, requires_lock: true do
+    state :clocked_in, initial: true
+    state :clocked_out
+
+    event :clock_out do
+      transitions from: :clocked_in, to: :clocked_out do
+        after do
+          self.clocked_out_at = Time.current
+          self.duration = (clocked_out_at - clocked_in_at).to_i
+        end
+      end
+    end
+  end
+
+  private
+
+  def valid_clock_out_time
+    errors.add(:clocked_out_at, "must be after the clock in time") if clocked_out_at <= clocked_in_at
+  end
+end
