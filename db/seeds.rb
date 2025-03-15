90253601d726a8a3cc8eecd4951a1fe1b6549edf
Windu Risky Adiatma <windu.risky@gmail.com>
2025-03-15 23:45:50 +0700

    script to add 10 million users with parallelism

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,4 +26,63 @@ User.create!(
   name: "Darth Vader"
 )
 
+# Adjust num_processes based on CPU cores
+def generate_ten_millions_user_data(num_processes: 6)
+  pids = []
+
+  num_processes.times do
+    pids << fork do
+      begin
+        ActiveRecord::Base.connection.reconnect! # Ensure DB connection works in forked process
+
+        (10_000_000 / num_processes).times do
+          start = 8.days.ago
+
+          user = User.create!(
+            username: Faker::Internet.username(specifier: 8..20),
+            password: "password",
+            name: Faker::Name.name
+          )
+
+          if rand(2) == 1 && base_user.followings.count < 10_000
+            Socials::FollowService.call(user: base_user, target_user_id: user.id)
+          end
+
+          1_000.times do
+            break if start > Time.current
+
+            duration = rand(8.hours.to_i)
+
+            sleep_record = SleepRecord.create!(
+              user: user,
+              clocked_in_at: start,
+              clocked_out_at: start + duration,
+              duration: duration,
+              state: :clocked_out
+            )
+            UpdateSleepTimelineJob.perform_now(sleep_record.id)
+
+            start += duration
+          end
+
+          puts "Created #{user.username} user data"
+        end
+      rescue StandardError => e
+        puts "Process #{Process.pid} encountered an error: #{e.message}"
+        puts e.backtrace.first(5) # Show first 5 lines of error trace
+
+      ensure
+        ActiveRecord::Base.connection.close # Close DB connection when done
+      end
+
+      exit # Make sure each process exits properly
+    end
+  end
+
+  # Wait for all forked processes to finish
+  pids.each { |pid| Process.wait(pid) }
+end
+
+generate_ten_millions_user_data
+
 puts "Seeds completed!"
