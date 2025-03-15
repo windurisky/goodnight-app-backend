e9735b9204abfd942581ff60cf34143e13afca68
Windu Risky Adiatma <windu.risky@gmail.com>
2025-03-15 12:46:37 +0700

    refactor logic in the job into service

--- app/jobs/update_sleep_timeline_job.rb
+++ app/jobs/update_sleep_timeline_job.rb
@@ -2,28 +2,6 @@ class UpdateSleepTimelineJob < ApplicationJob
   queue_as :default
 
   def perform(sleep_record_id)
-    sleep_record = SleepRecord.find_by(id: sleep_record_id)
-    return unless sleep_record&.clocked_out?
-
-    user = sleep_record.user
-    leaderboard_cache_key = "sleep_records_by_user_id:#{user.id}"
-    sleep_record_hash_key = "sleep_record_by_id:#{sleep_record.id}"
-
-    # Store in Redis sorted set (sorted by duration)
-    RedisService.add_to_sorted_set(
-      leaderboard_cache_key,
-      sleep_record.duration,
-      sleep_record.id
-    )
-
-    # Store detailed metadata in Redis Hash
-    RedisService.set_hash_field(sleep_record_hash_key, "user_id", user.id)
-    RedisService.set_hash_field(sleep_record_hash_key, "username", user.username)
-    RedisService.set_hash_field(sleep_record_hash_key, "clocked_in_at", sleep_record.clocked_in_at.iso8601)
-    RedisService.set_hash_field(sleep_record_hash_key, "clocked_out_at", sleep_record.clocked_out_at.iso8601)
-    RedisService.set_hash_field(sleep_record_hash_key, "duration", sleep_record.duration)
-
-    # Auto-expire the record in 7 days
-    RedisService.expire("sleep_record:#{sleep_record.id}", 7.days.to_i)
+    Sleep::UpdateTimelineService.call(sleep_record_id: sleep_record_id)
   end
 end
